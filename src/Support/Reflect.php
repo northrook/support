8bd3e0b29b1652a2b4475fd9cc7c15c9676a2fc4
Martin <hello@northrook.com>
2024-11-06 06:20:30 +0000

    mend

--- src/Support/Reflect.php
+++ src/Support/Reflect.php
@@ -4,29 +4,84 @@ declare(strict_types=1);
 
 namespace Support;
 
-use JetBrains\PhpStorm\Deprecated;
-use PHPStan\BetterReflection\Reflection\Adapter\ReflectionAttribute;
 use ReflectionClass;
 use BadMethodCallException;
 use ReflectionException;
+use ReflectionMethod;
 use Reflector;
 
+/**
+ * Helper methods for the native {@see Reflector} API.
+ */
 final class Reflect
 {
     /**
-     * @template T
+     * Assertive construction of a ReflectionClass.
+     *
+     * - Ensures the provided `class_exists`.
+     * - Wraps {@see ReflectionException} in a {@see BadMethodCallException}.
+     *
+     * @template T of ReflectionClass
+     *
+     * @param class-string<T>|T $class
+     *
+     * @return ReflectionClass<T>
+     */
+    public static function class( object|string $class ) : ReflectionClass
+    {
+        \assert( \class_exists( \is_object( $class ) ? $class::class : $class ) );
+
+        try {
+            return new ReflectionClass( $class );
+        }
+        catch ( ReflectionException $exception ) {
+            throw new BadMethodCallException( $exception->getMessage(), 500, $exception );
+        }
+    }
+
+    /**
+     * Assertive construction of a ReflectionClass.
+     *
+     *  - Ensures the provided `class_exists`.
+     *  - Wraps {@see ReflectionException} in a {@see BadMethodCallException}.
+     *
+     * @param class-string|object $class
+     * @param string              $method
+     *
+     * @return ReflectionMethod
+     */
+    public static function method( object|string $class, string $method ) : ReflectionMethod
+    {
+        \assert( \class_exists( \is_object( $class ) ? $class::class : $class ) );
+
+        try {
+            if ( $class instanceof ReflectionClass ) {
+                return $class->getMethod( $method );
+            }
+            return new ReflectionMethod( $class, $method );
+        }
+        catch ( ReflectionException $exception ) {
+            throw new BadMethodCallException( $exception->getMessage(), 500, $exception );
+        }
+    }
+
+    /**
+     * Retrieve a single {@see \Attribute} from a provided {@see Reflector instance}.
+     *
+     * Will throw {@see BadMethodCallException} if:
+     * - Passed `$reflector` doesn't provide `getAttributes`.
+     * - `$reflector` is missing the requested `$attribute`.
+     * - `$reflector` has multiple instances of requested `$attribute`.
+     *
+     * @template T of object
      *
      * @param Reflector       $reflector
      * @param class-string<T> $attribute
-     * @param bool            $asReflector
      *
-     * @return ($asReflector is true ? ReflectionAttribute[] : T)
+     * @return T
      */
-    public static function getAttribute(
-        Reflector $reflector,
-        string    $attribute,
-        bool      $asReflector = false,
-    ) : mixed {
+    public static function getAttribute( Reflector $reflector, string $attribute ) : object
+    {
         if ( ! \method_exists( $reflector, 'getAttributes' ) ) {
             throw new BadMethodCallException( "The passed reflector does not offer the 'getAttributes' method." );
         }
@@ -37,28 +92,6 @@ final class Reflect
             throw new BadMethodCallException( "The passed reflector does not offer the 'getAttributes' method." );
         }
 
-        return $asReflector ? $attributes[0] : $attributes[0]->newInstance();
-    }
-
-    /**
-     * Constructs a ReflectionClass.
-     *
-     * @link https://php.net/manual/en/reflectionclass.construct.php PHP Docs
-     *
-     * @template T of object
-     *
-     * @param class-string<T>|T $objectOrClass
-     *
-     * @return ReflectionClass<T>
-     */
-    #[Deprecated]
-    public static function class( object|string $objectOrClass ) : object
-    {
-        try {
-            return new ReflectionClass( $objectOrClass );
-        }
-        catch ( ReflectionException $exception ) {
-            throw new BadMethodCallException( $exception->getMessage(), 500, $exception );
-        }
+        return $attributes[0]->newInstance();
     }
 }
