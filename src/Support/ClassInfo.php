e956a877471cffa3f497bc12bae5e3a381751b84
Martin <hello@northrook.com>
2024-11-08 07:02:15 +0000

    mend

--- src/Support/ClassInfo.php
+++ src/Support/ClassInfo.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Support;
 
+use BadMethodCallException;
 use InvalidArgumentException;
 use Northrook\Filesystem\Path;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionMethod;
 
 final class ClassInfo
 {
@@ -18,6 +22,8 @@ final class ClassInfo
         'trait',
         'interface',
     ];
+
+    protected readonly ReflectionClass $reflection;
 
     /** @var array<int, string> */
     protected array $namespaces = [];
@@ -32,7 +38,7 @@ final class ClassInfo
 
     public readonly bool $exists;
 
-    public readonly Path $path;
+    public readonly ?Path $path;
 
     /** @var array<int, string> */
     protected array $types = [];
@@ -46,26 +52,96 @@ final class ClassInfo
     /** @var class-string[] */
     protected array $parents;
 
-    private function __construct(
+    /**
+     * @param class-string|Path|string $source
+     * @param bool                     $validate
+     */
+    public function __construct(
         Path|string $source,
         bool        $validate = false,
     ) {
-        if ( $source instanceof Path ) {
+        if ( $this->asSourceFilePath( $source ) ) {
             $this->path = $source;
             $this->parseFile();
+            $this->namespace = \implode( '\\', $this->namespaces ) ?: null;
+            $this->class     = \implode( '\\', [...$this->namespaces, $this->className] );
+        }
+        else {
+            $this->class = $source;
+            $filePath    = $this->reflect()->getFileName();
+            $this->path  = $filePath ? new Path( $filePath ) : null;
+            $source      = \explode( '\\', $source );
+
+            $this->className  = \array_pop( $source ) ?: throw new InvalidArgumentException();
+            $this->namespaces = $source;
+            $this->namespace  = \implode( '\\', $this->namespaces ) ?: null;
         }
 
         // find path to passed Namespace/Class
 
-        $this->namespace = \implode( '\\', $this->namespaces ) ?: null;
-
-        $this->class = \implode( '\\', [...$this->namespaces, $this->className] );
-
         $this->exists = \class_exists( $this->class );
 
         if ( $validate && ! $this->exists ) {
-            throw new InvalidArgumentException( "The class {$this->class} does not exists." );
+            throw new InvalidArgumentException( "The class {$this->class} cannot be loaded." );
         }
+    }
+
+    public function reflect() : ReflectionClass
+    {
+        try {
+            return $this->reflection ??= new ReflectionClass( $this->class );
+        }
+        catch ( ReflectionException $exception ) {
+            throw new BadMethodCallException( $exception->getMessage(), 500, $exception );
+        }
+    }
+
+    public function hasMethod( string ...$name ) : bool
+    {
+        foreach ( $name as $method ) {
+            if ( ! $this->reflect()->hasMethod( $method ) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function getMethod( string $name ) : ReflectionMethod
+    {
+        try {
+            return $this->reflect()->getMethod( $name );
+        }
+        catch ( ReflectionException $exception ) {
+            throw new BadMethodCallException( $exception->getMessage(), 500, $exception );
+        }
+    }
+
+    /**
+     * @param Path|string $source
+     *
+     * @phpstan-assert-if-false string $source
+     * @phpstan-assert-if-true Path    $source
+     * @return bool
+     */
+    private function asSourceFilePath( Path|string &$source ) : bool
+    {
+        if ( \is_string( $source ) && \str_ends_with( $source, '.php' ) ) {
+            $source = new Path( $source );
+        }
+
+        if ( $source instanceof Path ) {
+            if ( ! $source->exists() ) {
+                throw new InvalidArgumentException( "The provided path '{$source}' does not exist." );
+            }
+
+            if ( ! $source->isReadable ) {
+                throw new InvalidArgumentException( "The provided path '{$source}' is not readable." );
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     /**
@@ -128,33 +204,6 @@ final class ClassInfo
         return $this->parents ??= \class_parents( $this->class ) ?: [];
     }
 
-    public static function fromString( string $string, bool $strict = false ) : self
-    {
-        if ( \str_ends_with( $string, '.php' ) ) {
-            return self::fromFile( $string, $strict );
-        }
-        return new self( $string, $strict );
-    }
-
-    public static function fromFile( string $path, bool $strict = false ) : ClassInfo
-    {
-        $path = new Path( $path );
-
-        if ( ! $path->exists() ) {
-            throw new InvalidArgumentException( "The provided path '{$path}' does not exist." );
-        }
-
-        if ( ! $path->isReadable ) {
-            throw new InvalidArgumentException( "The provided path '{$path}' is not readable." );
-        }
-
-        if ( $strict && 'php' !== $path->extension ) {
-            throw new InvalidArgumentException( "The provided path '{$path}' must be to a PHP file." );
-        }
-
-        return new ClassInfo( $path, $strict );
-    }
-
     private function parseFile() : void
     {
         $filePath = (string) $this->path;
@@ -174,7 +223,7 @@ final class ClassInfo
             }
 
             if ( $this->lineContainsDefinition( $line ) ) {
-                $this->className = $this->parseClassName( $line );
+                $this->className = $this->setClassName( $line );
 
                 break;
             }
@@ -197,12 +246,12 @@ final class ClassInfo
         return false;
     }
 
-    private function parseClassName( string $line ) : string
+    private function setClassName( string $line ) : string
     {
         foreach ( $this::TYPES as $type ) {
             if ( \str_starts_with( $line, $type ) ) {
                 $this->types[] = $type;
-                return $this->parseClassName( \substr( $line, \strlen( $type ) + 1 ) );
+                return $this->setClassName( \substr( $line, \strlen( $type ) + 1 ) );
             }
         }
 
